4b912b754fc5bda0a3ef6457d81b1be2a9fa37d4
Alex <alexanderortiz.201@gmail.com>
2014-12-29 13:46:47 -0500

    Sort posts by submitted timestap.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,3 +1,5 @@
-Template.postsList.helpers({ posts: function() {
-  return Posts.find(); }
+Template.postsList.helpers({ 
+	posts: function() {
+  	return Posts.find({}, { sort: {submitted: -1} });
+  }
 });
